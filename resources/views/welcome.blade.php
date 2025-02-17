ca5df38d2cefb07912f4d06e7b5816b0f769fb8f
J.Alfredo <alfredoinver@gmail.com>
2025-02-16 23:53:33 -0600

    update: add Tailwind CSS CDN script with additional plugins to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,12 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
     </head>
     <body class="font-sans antialiased">
